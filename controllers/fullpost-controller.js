15961b5319536b87de26f0a59a71e1a1b98b7501
greatgreater <georgetaneetai@gmail.com>
2026-03-07 23:38:32 +0800

    feat: add render to post and comments in template

--- controllers/fullpost-controller.js
+++ controllers/fullpost-controller.js
@@ -1,9 +1,27 @@
 const Post = require('../models/Post-model');
 const Comment = require('../models/Comment-model');
+const User = require('../models/User-model');
 
-exports.getFullPost = (req, res) => {
+exports.getFullPost = async (req, res) => {
     // get post data from db
-    res.render('fullpost', {})
+    try {
+        const postId = req.query.id;
+
+        if (!postId) {
+            return res.status(400).send("Post ID missing.");
+        }
+
+        const [post, comments] = await Promise.all([
+            Post.findById(postId).populate('userId'),
+            Comment.find({ postId }).populate('userId').sort({ createdAt: -1})
+        ]);
+
+        res.render('fullpost', {post, comments, currentUser: ''});
+
+    } catch (err) {
+        console.error("Error: ", err);
+        return res.status(500).send("Internal Server Error");
+    }
 }
 
 exports.postComment = (req, res) => {
