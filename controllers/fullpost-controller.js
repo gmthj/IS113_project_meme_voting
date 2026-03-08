88254eb45d0d23579c91298c5bbf459ef577b992
greatgreater <georgetaneetai@gmail.com>
2026-03-08 14:06:39 +0800

    refactor: replace mongoose queries with service functions

--- controllers/fullpost-controller.js
+++ controllers/fullpost-controller.js
@@ -2,29 +2,27 @@ const Post = require('../models/Post-model');
 const Comment = require('../models/Comment-model');
 const User = require('../models/User-model');
 
-const { expandPosts } = require('../services/postService')
-const { expandComments } = require('../services/commentService')
+const { expandPosts, getPostById } = require('../services/postService')
+const { expandComments, getAllCommentsByPostId } = require('../services/commentService')
 
 exports.getFullPost = async (req, res) => {
     // get post data from db
     try {
-        // const postId = req.query.id;
         const postId = req.params.postId;
 
         if (!postId) {
             return res.status(400).send("Post ID missing.");
         }
 
-        const [rawPost, rawComments] = await Promise.all([
-            // Post.findById(postId).populate('userId'),
-            Post.findById(postId).lean(),
-            Comment.find({ postId }).sort({ createdAt: -1})
+        const [post, comments] = await Promise.all([
+            getPostById(postId),
+            getAllCommentsByPostId(postId)
         ]);
 
-        const posts = await expandPosts([rawPost]);
-        const comments = await expandComments(rawComments);
+        // const posts = await expandPosts([rawPost]);
+        // const comments = await expandComments(rawComments);
 
-        res.render('fullpost', {post: posts[0], comments, currentUser: ''});
+        res.render('fullpost', {post, comments, currentUser: ''});
 
     } catch (err) {
         console.error("Error: ", err);
