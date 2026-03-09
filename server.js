04cb63fd2f334d92d5a04b2f8e9bb42372088d90
Gabriel Tan <66828440+gmthj@users.noreply.github.com>
2026-03-09 21:36:11 +0800

    server port

--- server.js
+++ server.js
@@ -40,8 +40,8 @@ server.all('/:a', (req, res) => res.render('error', {error: "Unknown route"}));
 
 
 connectDB().then(() => { //startServer
-  const hostname = 'localhost'
-  const port = 8000
+  const hostname = 'localhost';
+  const port = process.env.PORT || 8000;
   server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`)
     // console.log(`dirname: ${__dirname}`)
