61d5be935e21cabc38f1da6d038ff57da3f6fd8c
xinlei <jeremy.zhaoxl@gmail.com>
2026-03-09 18:03:03 +0800

    created draft upload post route

--- routes/upload-route.js
+++ routes/upload-route.js
@@ -2,11 +2,13 @@ const express = require('express')
 const router = express.Router()
 const uploadController = require('../controllers/upload-controller')
 
+
 // =========================
 
 
 // /upload
 router.get('/', uploadController.renderUploadPage)
+router.post('/', uploadController.renderUploadPage_Mongo)
 
 
 
