74436df33d932e593aaed34295d4a09cfd4fbc94
zedwhyzy <zhiyu.lim.2025@computing.smu.edu.sg>
2026-03-10 15:26:29 +0800

    database

--- scripts/test-fetch.js
+++ scripts/test-fetch.js
@@ -1,6 +1,9 @@
 // scripts/test-fetch.js
 
 const mongoose = require("mongoose");
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config({ path: path.resolve(__dirname, "..", ".env") });
 const { connectDB } = require("../utils/utils");
 
 const User = require("../models/User-model");
