24937daf4d36bf7ceacbe545c2dbb4c60d43c0c3
hellokinyu <hellokinyu@gmail.com>
2026-03-22 23:25:01 +0800

    Render user info

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,9 +1,16 @@
-exports.renderUserProfile = (req, res) => {
+// Import user service
+const User = require('./../services/userService')
+
+exports.renderUserProfile = async (req, res) => {
     const sessionUser = req.session.sessionUser || {};
-    
     const userId = req.params.userId;
 
+    // Retrieve userInfo
+    let userInfo = await User.getUserById(userId);
+    console.log(userInfo);
 
 
-    res.render('user', {sessionUser})
+
+
+    res.render('user', {sessionUser, userInfo})
 }
