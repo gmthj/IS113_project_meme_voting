ac5dbcef9910c8863956c6c3356232fb8874d66c
hellokinyu <hellokinyu@gmail.com>
2026-03-23 01:00:48 +0800

    add getPostByUserId and render user's posts

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,16 +1,24 @@
 // Import user service
 const User = require('./../services/userService')
 
+// Import post service
+const { expandPosts, getPostById, getPostByUserId } = require('../services/postService')
+
 exports.renderUserProfile = async (req, res) => {
-    const sessionUser = req.session.sessionUser || {};
-    const userId = req.params.userId;
 
-    // Retrieve userInfo
-    let userInfo = await User.getUserById(userId);
-    console.log(userInfo);
-
-
-
-
-    res.render('user', {sessionUser, userInfo})
+    try {
+        const sessionUser = req.session.sessionUser || {};
+        const userId = req.params.userId;
+    
+        // Retrieve userInfo
+        let userInfo = await User.getUserById(userId);
+    
+        // Retrieve post info
+        let userPost = await getPostByUserId(userId, sessionUser)
+        console.log(userPost)
+    
+        res.render('user', {sessionUser, userInfo, userPost})
+    } catch (error) {
+        console.log("Error renderUserProfile:", error)
+    }
 }
--- services/postService.js
+++ services/postService.js
@@ -89,10 +89,23 @@ async function deletePostById(postId) {
 }
 
 
+// Get posts by userid
+async function getPostByUserId(userId, sessionUser) {
+  try {
+    let posts = await Post.find( {userId} ).lean()
+    return await expandPosts(posts, sessionUser)
+  } catch (error) {
+    console.log("Error finding post by userId:", error)
+  }
+}
+
+
+
 module.exports = {
   // expandPosts,
   getPostById,
   getAllPosts,
   getAllPostsSorted,
   deletePostById,
+  getPostByUserId,
 };
