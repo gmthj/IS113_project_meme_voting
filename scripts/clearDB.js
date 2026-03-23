e11f66f3e7c3bea8d55f1d49a5ec1e3d819b439e
Gabriel Tan <66828440+gmthj@users.noreply.github.com>
2026-03-23 21:11:56 +0800

    datav9 and purge db script

--- scripts/clearDB.js
+++ scripts/clearDB.js
@@ -1,24 +1,32 @@
+const dotenv = require("dotenv");
+dotenv.config();
 
 const mongoose = require("mongoose");
 const { connectDB } = require("../utils/utils");
 
-const User = require("../models/User-model");
-const Post = require("../models/Post-model");
-const Comment = require("../models/Comment-model");
-const Vote = require("../models/Vote-model");
+const User              = require("../models/User-model");
+const Post              = require("../models/Post-model");
+const Comment           = require("../models/Comment-model");
+const Vote              = require("../models/Vote-model");
+const Bookmark          = require("../models/Bookmark-model");
+const PostPreference    = require("../models/Post-Preference-model");
+const CommentPreference = require("../models/Comment-Preference-model");
 
 async function main() {
-  // 1) Connect
   await connectDB();
 
-  // 2) Ensure indexes
   await User.init();
   await Post.init();
   await Comment.init();
   await Vote.init();
+  await Bookmark.init();
+  await PostPreference.init();
+  await CommentPreference.init();
   console.log("Indexes ensured ✅");
 
-  // 3) Nuke everything
+  await CommentPreference.deleteMany({});
+  await PostPreference.deleteMany({});
+  await Bookmark.deleteMany({});
   await Vote.deleteMany({});
   await Comment.deleteMany({});
   await Post.deleteMany({});
@@ -26,12 +34,12 @@ async function main() {
   console.log("Collections cleared ✅");
 
   await mongoose.disconnect();
-  console.log("\nDisconnected ✅  Seed complete.");
+  console.log("\nDisconnected ✅");
   process.exit(0);
 }
 
 main().catch(async (e) => {
-  console.error("cleardb script error:", e);
+  console.error("clearDB error:", e);
   try { await mongoose.disconnect(); } catch {}
   process.exit(1);
 });
