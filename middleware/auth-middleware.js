2ed6e6cb991470637fdb1d3696ff5bdbebf07854
xinlei <jeremy.zhaoxl@gmail.com>
2026-03-30 18:18:31 +0800

    better isLoggin

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -1,14 +1,11 @@
-
-
 exports.isLoggedIn = (req, res, next) => {
-    // console.log("isloggedin")
-    // console.log(req.session)
-    if (!req.session.sessionUser._id) {
-        console.log("isLoggedIn - User not logged in, redirecting to /login");
-        return res.redirect('/account/login');
+    if (!req.session?.sessionUser?._id) {
+        console.log("isLoggedIn - User not logged in, redirecting to /account/login");
+        return res.redirect("/account/login");
     }
+
     next();
-}
+};
 
 
 exports.isAuthor = (req, res, next) => {
